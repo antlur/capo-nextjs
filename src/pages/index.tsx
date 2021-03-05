e4a61b38530387e46f8f5f40b2c876415e7303ff
Anthony Holmes <anthony@antlur.co>
2021-03-05 11:52:34 -0600

    intital commit of typescript and tailwind 2

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Capo Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      Hello
+    </div>
+  )
+}
